8a6a0ba2903b8c71a4c66fd6e7d8cf9f3b62576c
Maxime Bouillot <maxbouillot@gmail.com>
2026-01-21 20:04:26 +0100

    Standalone program can read files.

--- main.c
+++ main.c
@@ -1,80 +1,56 @@
 #include "SEForth.h"
+#include "stdlib.h"
 #include "stdio.h"
 
-#if 0
-extern const char* shell;
+
+static void parse_a_file(forth_state_t* fs, const char* file_name) {
+    FILE* f = fopen(file_name, "r");
+    if (f == NULL) {
+        fprintf(stderr, "Can't open file %s.\n", file_name);
+        exit(-1);
+    }
+    // We allocate a buffer that can contain the whole file.
+    // We expect the dictionary growth to be proportional to the file size so
+    // it should be fine.
+    fseek(f, 0L, SEEK_END);
+    size_t file_size = ftell(f);
+    rewind(f);
+
+    char* content = malloc(file_size);
+    if (content == NULL) {
+        fprintf(stderr, "Can't allocate bytes to read file.\n");
+        exit(-1);
+    }
+
+    fread(content, 1, file_size, f);
+    sef_parse_string(fs, content);
+    free(content);
+    fclose(f);
+}
 
 int main(int argc, char** argv) {
-    int rc = 0;
-    forth_state_t* fs = sef_init();
-#if SEF_USE_SOURCE_FILE
-    if (argc > 1) {
-#if SEF_PROGRAMMING_TOOLS
-        sef_feed_arguments(fs, argc - 1, argv + 1);
-#endif
-        if (sef_parse_file(fs, argv[1])) {
-            fprintf(stderr, "Error, unable to read file %s.\n", argv[1]);
-            return 1;
-        }
-#if SEF_PROGRAMMING_TOOLS
-        rc = sef_exit_code(fs);
-#endif
-        sef_free(fs);
-        return rc;
-    }
-#endif
-    sef_parse_string(fs, shell);
-#if SEF_PROGRAMMING_TOOLS
-    rc = sef_exit_code(fs);
-#endif
-    while (!sef_asked_bye(fs)) {
-        sef_restart(fs);
-        sef_parse_string(fs, "(repl) ");
-    }
-    sef_restart(fs);
-    sef_parse_string(fs, "(repl-buffer) 1- free ");
-    sef_free(fs);
-    return rc;
-}
-#endif
-
-int main(void) {
     static forth_state_t fs;
     sef_init(&fs);
-    sef_parse_string(&fs, " : test-prt .\" printing works if I get a 5 \" 5 . ; test-prt cr ");
-    printf("----\n");
-    sef_parse_string(&fs, " : test-literals 5 . 15 . 18 . ; test-literals cr");
-    printf("----\n");
-    sef_parse_string(&fs, " : test if .\" oui \" else .\" non \" then .\"  enfin \" cr ; ");
-    sef_parse_string(&fs, " 5 test 0 test");
-    printf("----\n");
-    sef_parse_string(&fs, ": test-loop 5 begin dup . dup while 1 - repeat cr ; test-loop ");
-    printf("----\n");
-    sef_parse_string(&fs, ": line1 1 . ;\n: line2 20 . ;\n: line3 300 . ;\nline3 line2 line1\ncr\n\n");
-    printf("----\n");
-    sef_parse_string(&fs, ": im-2 + . cr ; immediate : im-1 5 6 postpone im-2 ; immediate : nrm im-1 ;");
-    printf("----\n");
-    sef_parse_string(&fs, ": say-coucou .\" coucou\" cr ; : get-s s\" say-coucou\" ; get-s evaluate");
-    printf("----\n");
-    sef_parse_string(&fs, ": test-does create here ! 64 allot does> @ 5 + . ; 5 test-does abc abc cr ");
-    printf("----\n");
-    sef_parse_string(&fs, "variable my-var 55 my-var ! my-var @ . cr ");
-    printf("----\n");
-    sef_parse_string(&fs, ".\" Not calling words\" ( words) cr \\ cr cr cr cr");
-    printf("----\n");
-    sef_parse_string(&fs, ": msg .\" Printing second \".( Printing first ) cr ; msg");
-    printf("----\n");
-    sef_parse_string(&fs, ": test-case ( x -- ) case 5 of .\" five\" endof 6 of .\" six\" endof .\" nyet\" endcase cr ; 6 test-case 4 test-case 5 test-case");
-    printf("----\n");
-    sef_parse_string(&fs, ": test-i-j 3 0 do 4 0 do i . j . cr loop loop ; test-i-j");
-    printf("----\n");
-    sef_parse_string(&fs, ":noname .\" print twice \" ; dup execute execute cr");
-    printf("----\n");
-    sef_parse_string(&fs, ": recursive-loop ( x -- 0 ) dup . 1- dup if recurse then ; 5 recursive-loop . cr");
-    printf("----\n");
-    sef_parse_string(&fs, ": test-c c\" counted string work!\" count type cr ; test-c");
-    printf("----\n");
-    sef_parse_string(&fs, "0 value v : up v . 5 to v ; up v . 8 to v up cr");
+
+#if SEF_ARG_AND_EXIT_CODE
+    if (argc > 1) {
+        sef_feed_arguments(&fs, argc - 1, argv + 1);
+    }
+#endif
+
+    if (argc > 1) {
+        parse_a_file(&fs, argv[1]);
+    } else {
+        fprintf(stderr, "TODO\n");
+        exit(-1);
+    }
+
+#if SEF_ARG_AND_EXIT_CODE
+    // TODO: check that we are not compiling. Maybe calling quit is enough
+    sef_parse_string(&fs, "exit-code @");
+    return sef_pop_data(&fs);
+#else
     return fs.error_encountered;
+#endif
 }
 
